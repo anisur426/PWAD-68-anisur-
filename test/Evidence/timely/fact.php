00a8fb484b39949168ce2793aa05b23ac3376f3d
anisur426 <rahmanmdanisur069@gmail.com>
2025-11-16 13:05:45 +0600

    class

--- test/Evidence/timely/fact.php
+++ test/Evidence/timely/fact.php
@@ -1,18 +1,32 @@
-<form method="post">
-    <label for=""> Enter Number:</label>
-    <input type="number" name="num"><br>
-    <input type="submit" name="submit" value="Check">
-</form>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-if (isset($_POST["submit"])) {
-    $num = $_POST["num"];
-    $fact = 1;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    for ($i = $num; $i >= 1; $i--) {
-        $fact = $fact * $i;
+<body>
+    <form method="post">
+        <label for=""> Enter Number:</label>
+        <input type="number" name="num"><br>
+        <input type="submit" name="submit" value="Check">
+    </form>
+
+    <?php
+    if (isset($_POST["submit"])) {
+        $num = $_POST["num"];
+        $fact = 1;
+
+        for ($i = $num; $i >= 1; $i--) {
+            $fact = $fact * $i;
+        }
+
+        echo "Factorial of $num is: $fact";
     }
+    ?>
 
-    echo "Factorial of $num is: $fact";
-}
-?>
+</body>
+
+</html>
