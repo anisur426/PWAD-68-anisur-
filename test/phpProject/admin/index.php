b2c2cd5d3c6f676d19445b6a1810a1651629e979
anisur <rahmanmdanisuro69@gmail.com>
2025-11-27 08:07:22 +0600

    home solkal

--- test/phpProject/admin/index.php
+++ test/phpProject/admin/index.php
@@ -1,6 +1,6 @@
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,103 +15,102 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
-
 <body class="hold-transition login-page">
-  <div class="login-box">
-    <?php if (isset($_POST['login'])) {
-      include_once("inc/db_config.php");
-      extract($_POST);
-      $password = md5($password);
-      $sql = "SELECT * FROM admins WHERE email='$email' AND password='$password'";
-      $record = $db->query($sql);
-      $row = $record->fetch_assoc();
-      //echo $record->num_rows;
-      if ($record->num_rows > 0) {
-        session_start();
-        $_SESSION['loggedin'] = TRUE;
-        $_SESSION['email'] = $email;
-        $_SESSION['name'] = $row['name'];
-        $_SESSION['photo'] = $row['photo'];
+<div class="login-box">
+  <?php if(isset($_POST['login'])){
+    include_once("inc/db_config.php");
+    extract($_POST);
+    $password = md5($password);
+    $sql = "SELECT * FROM admin WHERE email='$email' AND password='$password'";
+   $rawdata = $conn->query($sql);
+    $row  =$rawdata->fetch_assoc();
+    //ech $rawdata->num_rows;
+    if($rawdata->num_rows > 0){
+      session_start();
+      $_SESSION['loggedin'] = TRUE ;
+      $_SESSION['email'] = $Email ;
+      $_SESSION['name'] = $row['name'];
+      $_SESSION['photo'] = $row['photo'];
 
-        header("Location:dashboard.php");
-      } else {
-        echo $record->num_rows;
-      }
+      header("Location:dashboard.php");
     }
-    ?>
-    <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
-      <div class="card-header text-center">
-        <a href="index2.html" class="h1"><b>LOGIN Form</b></a>
-      </div>
-      <div class="card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-
-        <form action="" method="post">
-
-          <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-8">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember">
-                  Remember Me
-                </label>
-              </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
-
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
-
-        <div class="social-auth-links text-center mt-2 mb-3">
-          <a href="#" class="btn btn-block btn-primary">
-            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-block btn-danger">
-            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-          <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-          <a href="register.html" class="text-center">Register a new membership</a>
-        </p>
-      </div>
-      <!-- /.card-body -->
+    else{
+      echo $rawdata->num_rows;
+    }
+  } 
+  ?>
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="index2.html" class="h1"><b>LOGIN Form</b></a>
     </div>
-    <!-- /.card -->
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="" method="post">
+        
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+            
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center mt-2 mb-3">
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p>
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Register a new membership</a>
+      </p>
+    </div>
+    <!-- /.card-body -->
   </div>
-  <!-- /.login-box -->
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
 </body>
-
 </html>
